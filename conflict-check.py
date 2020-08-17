d7d45ef768ef2b6a1abf90a0c05179644632a07c
ankur8931 <achaud16@asu.edu>
2020-08-17 11:34:13 -0700

    code changes to conflict checking

--- conflict-check.py
+++ conflict-check.py
@@ -64,7 +64,22 @@ def conflict_detection(flows):
                a_1 = f_1['action']
                a_2 = f_2['action']
              
+               f_1proto = ''
+               f_2proto = ''
 
+               # Extract the protocol
+               if 'TCP_SRC' or 'TCP_DST' in f_1.keys():
+                   f1_proto = 'TCP'
+               if 'UDP_SRC' or 'UDP_DST' in f_1.keys():
+                   f1_proto = 'UDP'               
+               if 'TCP_SRC' or 'TCP_DST' in f_2.keys():
+                   f2_proto = 'TCP'
+               if 'UDP_SRC' or 'UDP_DST' in f_2.keys():
+                   f2_proto = 'UDP'
+               
+               if f1_proto != f2_proto:
+                  print ("No conflict between R1 and R2")
+                         
                # Extract rule1, rule2 layer2 fields
                f_1l2src = ''
                f_1l2dst = ''
@@ -104,37 +119,80 @@ def conflict_detection(flows):
                f_2l4src = ''
                f_2l4dst = ''
 
-                
-               if 'TCP_SRC' in f_1.keys():
-                   f_1l4src = f_1['TCP_SRC']
-               if 'TCP_DST' in f_1.keys():
-                   f_1l4dst = f_1['TCP_DST']
-               if 'TCP_SRC' in f_2.keys():
-                   f_2l4src = f_2['TCP_SRC']
-               if 'TCP_DST' in f_2.keys():
-                   f_2l4dst = f_2['TCP_DST']
-
-               # Check for inheritance conflict
-               if ((isL2subsetr2r1(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
-                   isL2equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst))
-                   and (isL3subsetr2r1(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
-                   isL3equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst))
-                   and (isL4subsetr2r1(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
-                   isL4equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst))):
+               if  f_1proto == 'TCP' and f_2proto == 'TCP': 
+                   if 'TCP_SRC' in f_1.keys():
+                       f_1l4src = f_1['TCP_SRC']
+                   if 'TCP_DST' in f_1.keys():
+                       f_1l4dst = f_1['TCP_DST']
+                   if 'TCP_SRC' in f_2.keys():
+                       f_2l4src = f_2['TCP_SRC']
+                   if 'TCP_DST' in f_2.keys():
+                       f_2l4dst = f_2['TCP_DST']
+               elif f_1proto == 'UDP' and  f_2proto == 'UDP':
+                   if 'UDP_SRC' in f_1.keys():
+                       f_1l4src = f_1['UDP_SRC']
+                   if 'UDP_DST' in f_1.keys():
+                       f_1l4dst = f_1['UDP_DST']
+                   if 'UDP_SRC' in f_2.keys():
+                       f_2l4src = f_2['UDP_SRC']
+                   if 'UDP_DST' in f_2.keys():
+                       f_2l4dst = f_2['UDP_DST']
                     
-                     print("Inheritance: R2 is subset of R1")
-                                     
-               elif ((isL2subsetr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
+               # Check if rule2 is subset or equals rule1
+
+               bool l2overlap = False, l3overlap = False, l4overlap = False
+
+               if (isL2subsetr2r1(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
                    isL2equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst))
-                   and (isL3subsetr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
-                   isL3equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst))
-                   and (isL4subsetr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
-                   isL4equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst))):
-                     
-                     print("Inheritance: R2 is subset of R1")
+                   l2overlap = True
+                   
+               if(isL3subsetr2r1(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
+                   isL3equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst)):
+                   l3overlap = True
 
-                                 
+               if(isL4subsetr2r1(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
+                   isL4equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst)):
+                   l4overlap = True
+  
+                   if l2overlap and l3overlap and l4overlap:
+                      if a_1 == a_2:
+                         print("Inheritance: R2 is subset of R1 same action")
+                      else:
+                         print("Polymorphism: R2 subset of R1 but different actions")
+                   if l2overlap or l3overlap or l4overlap:
+                       if a_1 == a_2:
+                         print("Aggregation: R2 overlaps R1 same action")
+                      else:
+                         print("Composition: R2 overlaps R1 different action")
 
+               # Check if rule1 is subset or equals rule2
+ 
+               if (isL2subsetr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
+                   isL2equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst))
+                   l2overlap = True
+                            
+               if(isL3subsetr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
+                   isL3equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst)):
+                   l3overlap = True
+
+               if(isL4subsetr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst) or
+                   isL4equalsr1r2(f_1l2src, f_1l2dst, f_2l2src, f_2l2dst)):
+                   l4overlap = True
+  
+                   if l2overlap and l3overlap and l4overlap:
+                      if a_1 == a_2:
+                         print("Inheritance: R1 is subset of R2 same action")
+                      else:
+                         print("Polymorphism: R1 subset of R2 but different actions")
+                   if l2overlap or l3overlap or l4overlap:
+                       if a_1 == a_2:
+                         print("Aggregation: R1 overlaps R2 same action")
+                      else:
+                         print("Composition: R1 overlaps R1 different action")
+
+               # Check for partial overlap                  
+                           
+               
 flow1 = "http://192.168.3.30:8181/onos/v1/flows/"
 r = requests.get(flow1, auth=('onos','rocks'))
 flows = list()
